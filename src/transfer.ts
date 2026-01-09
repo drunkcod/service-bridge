3752db931dda3d323a1378433becafc99cc2825a
Tobbe Gyllebring <tobbe@hubert.ai>
2026-01-09 15:22:02 +0100

    Better transfer typing.

--- src/transfer.ts
+++ src/transfer.ts
@@ -6,7 +6,10 @@ export type Transfer<T> = { [Transfer]: true; readonly value: T; readonly list?:
 
 export type Transferred<T> = T & { [Transfer]: false };
 
-export const transfer = <T>(value: T, list?: Transferable[]) => ({ [Transfer]: true, value, list } as const);
+export const transfer: {
+	<T extends Transferable>(value: T): Transfer<T>;
+	<T>(value: T, list: Transferable[]): Transfer<T>;
+} = <T>(value: T, list?: Transferable[]) => ({ [Transfer]: true, value, list } as const);
 
 export function isTransfer(x: unknown): x is Transfer<unknown> {
 	return !!x && typeof x === 'object' && Transfer in x;
